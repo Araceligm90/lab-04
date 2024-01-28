e2dddf0bb1f3d47c9f521609c86f3b88078c9d94
Araceligm90 <garcia.araceli90@gmail.com>
2024-01-28 10:38:21 +0100

    calculus

--- main.py
+++ main.py
@@ -22,3 +22,24 @@ def text_length(text: str):
         "length": len(text) 
         }
 
+@app.get("/add")
+def add(num1: int, num2: int):
+    return {"result": num1 + num2}
+
+@app.get("/subtract")
+def subtract(num1: int, num2: int):
+    return {"result": num1 - num2} 
+    
+
+@app.get("/multiply")
+def multiply(num1: int, num2: int):
+    return {"result": num1 * num2} 
+
+@app.get("/divide")
+def divide(num1: int, num2: int):
+  if num2 == 0:
+    return {"error: cannot divide by 0"}
+  return {"result": num1 // num2}
+
+
+## uvicorn main:app --reload <-- to reload page 
